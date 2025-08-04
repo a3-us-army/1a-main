3df24d2f3676ff2556febbe114c82bb43999ace5
Xander <main@xanderxx.xyz>
2025-08-04 23:51:46 +0000

    Make equipment be admin only, and make profile buttons look better.

--- src/website/routes/admin/index.js
+++ src/website/routes/admin/index.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import galleryAdminRoutes from './gallery-admin.js';
 import applicationRoutes from './application.js';
 import vpsStatusRoutes from './vps-status.js';
+import equipmentRoutes from './equipment.js';
 
 const router = Router();
 
 router.use('/gallery-admin', galleryAdminRoutes);
 router.use('/apply', applicationRoutes);
 router.use('/vps-status', vpsStatusRoutes);
-
+router.use('/equipment', equipmentRoutes);
 export default router; 
--- src/website/routes/content/index.js
+++ src/website/routes/content/index.js
@@ -5,7 +5,6 @@ import certsRoutes from './certs.js';
 import personnelRoutes from './personnel.js';
 import loaRoutes from './loa.js';
 import modpacksRouter from './modpack.js';
-import equipmentRoutes from './equipment.js';
 import formsRoutes from './forms.js';
 
 const router = Router();
@@ -16,7 +15,6 @@ router.use('/certs', certsRoutes);
 router.use('/personnel', personnelRoutes);
 router.use('/loa', loaRoutes);
 router.use('/modpacks', modpacksRouter);
-router.use('/equipment', equipmentRoutes);
 router.use('/forms', formsRoutes);
 
 export default router; 
